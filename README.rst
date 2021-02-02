57b5792272ee3c6749d4df5b0d23a7fcd8aa7b9a
Richard McDonald <rmcd@usgs.gov>
2021-02-02 14:57:59 -0700

    added cross-section tool

--- README.rst
+++ README.rst
@@ -18,8 +18,13 @@ NLDI Elevation Services
 
 NLDI Elevation Sevices
 
+Development
+-----------
+* conda env create -f .\requirements_dev.yml
+* pip install -e .
 
-* Free software: MIT license
+
+* Free software
 * Documentation: https://nldi-el-serv.readthedocs.io.
 
 
@@ -31,7 +36,22 @@ Features
 Credits
 -------
 
+CLI developed from example: https://github.com/pallets/click/blob/master/examples/repo/repo.py
+
 This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
 
 .. _Cookiecutter: https://github.com/audreyr/cookiecutter
 .. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+
+Disclaimer
+----------
+
+This software is preliminary or provisional and is subject to revision. It is
+being provided to meet the need for timely best science. The software has not
+received final approval by the U.S. Geological Survey (USGS). No warranty,
+expressed or implied, is made by the USGS or the U.S. Government as to the
+functionality of the software and related material nor shall the fact of release
+constitute any such warranty. The software is provided on the condition that
+neither the USGS nor the U.S. Government shall be held liable for any damages
+resulting from the authorized or unauthorized use of the software.
+
--- nldi_el_serv/cli.py
+++ nldi_el_serv/cli.py
@@ -1,15 +1,85 @@
 """Console script for nldi_el_serv."""
 import sys
 import click
+import numpy as np
+from nldi_el_serv.nldi_el_serv import getXSAtPoint
+import geopandas as gpd
+import pandas as pd
+import json
+
+class NLDI_El_Serv:
+    def __init__(self):
+        self.out_crs = "epsg:4326"
+
+    def setoutCRS(self, out_crs="epsg:4326"):
+        self.out_crs = out_crs
+
+    def outCRS(self):
+        return self.out_crs
+
+    def __repr__(self):
+        return f"NLDI_El_Serv {self.out_crs}"
 
 
-@click.command()
-def main(args=None):
-    """Console script for nldi_el_serv."""
-    click.echo("Replace this message by putting your code into "
-               "nldi_el_serv.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
+pass_nldi_el_serv = click.make_pass_decorator(NLDI_El_Serv)
+
+
+@click.group()
+@click.option("--outcrs",
+              default="epsg:4326",
+              help="Projection CRS to return cross-section geometry: default is epsg:4326")
+@click.version_option("0.1")
+@click.pass_context
+def main(ctx, outcrs):
+    '''NLDI_El_Serv is a command line tool to for elevation-based services to the NLDI'''
+
+    ctx.obj = NLDI_El_Serv()
+    ctx.obj.setoutCRS(outcrs)
     return 0
+
+
+@main.command()
+@click.option("--file",
+              required=True,
+              type=click.File('w'),
+              help='Output json file')
+@click.option("--latlon",
+              required=True,
+              # nargs=2,
+              type=tuple((np.float, np.float)),
+              # default=(-103.80119199999999, 40.268403),
+              help="format lat lon as floats for example lat lon or -103.8011 40.2684")
+@click.option("--numpoints",
+             default=100,
+             type=int,
+             help="number of points in cross-section")
+@click.option("--width",
+              default=1000.0,
+              type=float,
+              help="width of cross-section")
+
+@pass_nldi_el_serv
+def XSAtPoint(nldi_el_serv, latlon, numpoints, width, file):
+    lat = latlon[0]
+    lon = latlon[1]
+    print(f'input={latlon}, lat={lat}, lon={lon}, \
+    npts={numpoints}, width={width} and crs={nldi_el_serv.outCRS()} and \
+    file={file} and out_epsg={nldi_el_serv.outCRS()}')
+    # print(tuple(latlon))
+    xs = getXSAtPoint(point=tuple((lat, lon)),
+                      numpoints=numpoints,
+                      width=width,
+                      file=file)
+    # jsdict = xs.to_crs(xstool.out_crs).to_dict() #.to_crs(xstool.out_crs)
+    # # print(type(jsdict))
+    # # print(jsdict)
+    # json.dump(xs.to_dict(), file)
+    return xs
+    # df = pd.DataFrame({'elevation':xs.elevation.values,
+    #                    'Latitude':xs.coords.y.values,
+    #                    'Longitude':xs.coords.x.values})
+    # gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.Longitude, df.Latitude))
+    # return gdf.to_json()
 
 
 if __name__ == "__main__":
