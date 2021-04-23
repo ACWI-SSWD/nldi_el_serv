21338db6f7bca37b5ac10bf807944d75f695a90a
Richard McDonald <rmcd@usgs.gov>
2021-04-23 15:36:39 -0600

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ACWI-SSWD/nldi_el_serv',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
